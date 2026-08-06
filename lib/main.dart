58d091e6cf642584e1a25982f4f1217458790a72
unknown <ihnumscacu@gmail.com>
2026-08-07 05:08:54 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:workshop3_122/screen/itemfriends.dart';
+import 'package:workshop3_122/screen/addform.dart';
 //import 'screen/home.dart';
 //import 'screen/item.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AddForm());
 }
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 240, 198, 198),
         appBar: AppBar(
           title: const Text('CET App ID:122'),
           backgroundColor: Colors.orange,
